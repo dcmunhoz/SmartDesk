b10aebaa2fe3831a9e2485deabe7ad7cf5b67a5e
dcmunhoz <dc.munhoz@hotmail.com>
2019-05-29 22:28:43 -0300

    Estilo abertura de chamado com campos iniciais

--- views/user-ticket.php
+++ views/user-ticket.php
@@ -3,8 +3,34 @@
 </header>
 <div class="open-ticket-panel">
     <form action="#" method="#">
-    
-    
-        
+        <div class="row row-2">
+            <div class="form-group">
+                <label for="ticket-title">Titulo do ticket:</label>
+                <input type="text" name="ticket-title" id="ticket-title" class="dark" placeholder="Breve descrição do problema.">
+            </div>
+
+            <div class="form-group">
+                <label for="ticket-priority">Titulo do ticket:</label>
+                <select name="ticket-priority" id="ticket-priority" class="dark">
+                    <option value="">Prioridade</option>
+                    <option value="">Prioridade</option>
+                    <option value="">Prioridade</option>
+                    <option value="">Prioridade</option>
+                    <option value="">Prioridade</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group">
+                <label for="ticket-desc">Descrição do ticket:</label>
+                <textarea name="ticket-desc" id="ticket-desc" class="dark" placeholder="Descreva aqui o ocorrido detalhadamente..." cols="1" rows="10" ></textarea>
+            </div>
+        </div>
+
+        <footer class="form-footer">
+            <button class="btn btn-ok">Enviar Ticket</button>
+        </footer>
+
     </form>
 </div>
