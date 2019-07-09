5912a89dd268396bd0b41d5590f2f66d1de36139
dcmunhoz <dc.munhoz@hotmail.com>
2019-07-09 01:03:36 -0300

    Iniciando paginas de admin

--- routes/admin.php
+++ routes/admin.php
@@ -7,9 +7,18 @@
  */
 
 use \App\View as View;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
-// Pagina inicial do admin.
 $app->get("/admin", function(){
+
+    $view = new View(false, false);
+    $view->draw('admin-home');
+
+});
+
+// Pagina login do admin.
+$app->get("/admin/signin", function(){
 
     $view = new View(false, false);
 
